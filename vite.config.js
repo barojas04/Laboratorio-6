6d1f9d60fe405c3b4a569e98d3dcb993e5ddf423
barojas04 <barojas04@gmail.com>
2026-04-30 14:45:34 -0600

    cambio de rutas

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src',
+  root: "./src",
+  base: "/Laboratorio-6/",
+  publicDir: "../public",
+  plugins: [],
+  server: { port: 1234 },
   build: {
-    outDir: '../dist',
-    emptyOutDir: true,
+    outDir: "../dist",
+    emptyOutDir: true
   }
 });
